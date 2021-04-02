07f3fbe5b259810060d3fe3ddfdfb47742458425
박동수 <floreerin@gmail.com>
2021-04-02 11:19:28 +0900

    kotiln 플러그인 연결 (InteilJ), 두개 뽑아서 더하기 문제(프로그래머스) 테스트 input, output 설정

--- algorithm/programmas/Level1_TwoPlus.kt
+++ algorithm/programmas/Level1_TwoPlus.kt
@@ -2,24 +2,41 @@ class Level1_TwoPlus {
     fun solution(numbers: IntArray): IntArray{
         val list = arrayListOf<Int>()
 
-
         for (i in 0 until numbers.size){
             for (j in i+1 until numbers.size) {
                 val plus = numbers[i] + numbers[j]
                 if (!list.contains(plus)){ // contains는 해당 배열에 추출된 값이 있는지 확
-                    list.add(plus)
+                    list.add(plus) // 없다면 추가
                 }
             }
         }
 
-        list.sort()
+        list.sort() // 정렬
 
         val answer = IntArray(list.size)
 
         for (k in 0 until list.size){
-            answer[k] = list.get(k)
+            answer[k] = list[k]
         }
 
         return answer
+    }
+}
+
+fun main() {
+    val solution = Level1_TwoPlus()
+
+    val input : IntArray = intArrayOf(2,1,3,4,1)
+    val output = solution.solution(input)
+    for (i in 0 until output.size){
+        print("${output[i]}") // 2,3,4,5,6,7
+    }
+
+    print("\n")
+
+    val input2 : IntArray = intArrayOf(5,0,2,7)
+    val output2 = solution.solution(input2)
+    for (i in 0 until output2.size){
+        print("${output2[i]}") // 2,5,7,9,2
     }
 }
