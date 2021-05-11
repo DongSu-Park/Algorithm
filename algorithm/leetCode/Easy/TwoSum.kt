6bee5aa1ede28b8dd7553200b9ea748ee1966fd1
박동수 <floreerin@gmail.com>
2021-05-11 14:41:22 +0900

    Easy - Two Sum 문제 풀이 (Success, 200ms)

---
+++ algorithm/leetCode/Easy/TwoSum.kt
@@ -0,0 +1,38 @@
+/**
+ * Problems : Two Sum
+ * Difficulty : Easy
+ * Url : https://leetcode.com/problems/two-sum/
+ *
+ * 문제 설명
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ *  -> 배열이 주어졌을 때 두 개를 고르고 더했을 때 target의 숫자와 일치한 배열의 위치를 리턴
+ *
+ *
+ * */
+
+class TwoSum {
+    fun solution(nums: IntArray, target: Int): IntArray{
+        val answer = IntArray(2)
+
+        for (i in 0 until nums.size){
+            for (j in i+1 until nums.size){
+                if (nums[i] + nums[j] == target){
+                    answer[0] = i
+                    answer[1] = j
+                }
+            }
+        }
+
+        return answer
+    }
+}
+
+fun main() {
+    val solution = TwoSum()
+
+    println(solution.solution(intArrayOf(2,7,11,15), 9).toList())
+    println(solution.solution(intArrayOf(3,2,4),6).toList())
+    println(solution.solution(intArrayOf(3,3), 6).toList())
+}
